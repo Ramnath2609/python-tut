2cbd0ca850fe691279d473f2e1b3996aca9379c8
Ramnath2609 <ramnath2609@gmail.com>
2025-06-07 12:35:15 +0530

    initial commit

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+print("Hello world")
+a = [1, 3, 5]
+
+print(a)
+
+a.append(4)
+
+print(a)
+
+print("Length of the array is", len(a))
+
+if 6 in a:
+    print('Yes, element is present')
+else:
+    print("Element not present")
